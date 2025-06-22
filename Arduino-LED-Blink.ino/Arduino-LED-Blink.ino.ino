f4f09bf5bd8f9a9fb1ccc905eccbb249b237496e
Minidu Chelaka <miniduchelakaofficial@gmail.com>
2025-06-22 19:56:29 +0530

    Add files via upload
    Add Arduino LED blink code

---
+++ Arduino-LED-Blink.ino/Arduino-LED-Blink.ino.ino
@@ -0,0 +1,11 @@
+void setup() {
+  pinMode(13, OUTPUT);
+}
+
+void loop() {
+  digitalWrite(13, HIGH); // turn the LED on
+  delay(1000);            // wait 1 second
+  digitalWrite(13, LOW);  // turn the LED off
+  delay(1000);            // wait 1 second
+}
+
